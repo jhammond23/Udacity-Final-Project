01bd31d69f3e8006c43825b0743a552a1faf888d
jhammond23 <108297205+jhammond23@users.noreply.github.com>
2022-09-14 13:07:31 -0500

    Updated base url to '.io'

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -1,5 +1,5 @@
 /* Create an array named products which you will use to add all of your product object literals that you create in the next step. */
-let base = "jhammond23.github.io"
+let base = "https://jhammond23.github.io/jhammond23.github.io/starter/src/assets/script.js"
 const products = [
   {
     name: 'Curved Handle Steel Axe',
